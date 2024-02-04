844a694e849a226d5ae9bb0fac21b780a9c96164
Hibari1020 <118356430+Hibari1020@users.noreply.github.com>
2024-02-04 13:13:31 +0000

    Make Session Controler

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root "static_pages#start"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
