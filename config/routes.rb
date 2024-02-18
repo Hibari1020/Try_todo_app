f46abc6b0b0835867ded4eef148ae0851560bd51
Hibari1020 <hirokiyokfujita@icloud.com>
2024-02-18 15:38:40 +0900

    sharing Task function

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,22 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts,  only: [:create] do
     member do
       post 'update_done'
       post 'update_not_done'
+      post 'update_share'
+      post 'update_not_share'
     end
   end
+  resources :relationships,       only: [:create, :destroy]
   get '/microposts', to: "static_pages#start"
   get "/microposts_done_list", to: "microposts#done_index", as: "done_index"
 end
